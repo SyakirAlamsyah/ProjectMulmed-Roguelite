ccccf417e5362ea1128b541352adef3c9d0c2165
Chiwassu <kristoforusnoventa@gmail.com>
2026-06-26 20:58:25 +0700

    movement

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D rb;
+    Vector2 mdir;
+
+    void inputmanagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        mdir = new Vector2(moveX, moveY).normalized;
+    }
+
+    void movement()
+    {
+        rb.velocity = new Vector2(mdir.x * speed, mdir.y * speed);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputmanagement();
+    }
+
+    private void FixedUpdate()
+    {
+        movement();
+    }
+}
