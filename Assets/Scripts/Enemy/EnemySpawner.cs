7cbf1ca111a3a1bb2c97a5c8dd2f1972654de09c
Chiwassu <kristoforusnoventa@gmail.com>
2026-07-02 12:26:04 +0700

    "Roguelike" done

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -5,27 +5,78 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [System.Serializable]
-    public class wave
+    public class Wave
     {
         public string waveName;
-        public List<GameObject> enemyPrefabs; //list of enemy prefabs to spawn in this wave
-        public List<string> enemyName;
-        public List<int> enemyCount;
-        public int waveQuota; //number of enemies to spawn in this wave
-        public float spawnInterval; //time between spawns
-        public float spawnCount; //number of enemies already spawned in this wave
+        public List<EnemyGroup> EnemyGroups;
+        public int waveQuota;
+        public float waveInterval;
+        public int spawnCount;
     }
 
-    public List<wave> waves; //list of waves to spawn
+    [System.Serializable]
+    public class EnemyGroup
+    {
+        public string enemyname;
+        public int enemycount;
+        public int spawncount;
+        public GameObject enemyPrefab;
+    }
 
+    public List<Wave> waves;
+    public int currentWaveCount;
+
+    [Header("Wave Settings")]
+    float spawnTime;
+
+    Transform player;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        CalculateWaveQuota();
+        player = FindObjectOfType<PlayerStats>().transform; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        spawnTime += Time.deltaTime;
+
+        if(spawnTime >= waves[currentWaveCount].waveInterval)
+        {
+            spawnTime = 0f;
+            SpawnEnemy();
+        }
+    }
+
+    void CalculateWaveQuota()
+    {
+        int currentWaveQuota = 0;
+        foreach (var EnemyGroups in waves[currentWaveCount].EnemyGroups)
+        {
+            currentWaveQuota += EnemyGroups.enemycount;
+        }
+        waves[currentWaveCount].waveQuota = currentWaveQuota;
+        Debug.Log("Current Wave Quota: " + currentWaveQuota);
+    }
+
+    void SpawnEnemy()
+    {
+        if (waves[currentWaveCount].spawnCount < waves[currentWaveCount].waveQuota)
+        {
+            foreach (var EnemyGroups in waves[currentWaveCount].EnemyGroups)
+            {
+                if (EnemyGroups.spawncount < EnemyGroups.enemycount)
+                {
+                    Vector2 spawnPosition = new Vector2(player.transform.position.x + Random.Range(-10f, 10f), player.transform.position.y + Random.Range(-10f, 10f));
+                    Instantiate(EnemyGroups.enemyPrefab, spawnPosition, Quaternion.identity);
+
+                    EnemyGroups.spawncount++;
+                    waves[currentWaveCount].spawnCount++;
+                }
+            }
+        }
     }
 }
